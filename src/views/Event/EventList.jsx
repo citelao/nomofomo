d59d5425d3f86784df0dc780ee59adb15e9fa5bb
Ben <ben@stolovitz.com>
2015-09-19 16:37:36 -0400

    don't delete things tho

---
+++ src/views/Event/EventList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import Event from "../../modules/event";
+
+export default class EventList extends React.Component {
+	render() {
+		return <div><Event /></div>;
+		// return <Event />;
+	}
+}
