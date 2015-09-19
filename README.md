850e88ffa0cfd39cadfd7a85d8387388de78cd8b
Ben <ben@stolovitz.com>
2015-09-19 09:31:31 -0400

    webpacking things :)

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 Because other people want to hang out with you.
 
 [![Build Status](https://travis-ci.org/citelao/nomofomo.svg?branch=master)](https://travis-ci.org/citelao/nomofomo)
+
+## Building
+
+`gulp` compiles into `dist/`
+
+`gulp serve` launches a server on `8080`.
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,47 @@
+/* eslint-env node */
+import path from "path";
+
 import gulp from "gulp";
 import gutil from "gulp-util";
+import webpack from "webpack";
+import webpackDevServer from "webpack-dev-server";
 
-gulp.task("default", function() {
-	gutil.log("compiled");
+const webpackOptions = {
+	entry: "./src/index.js",
+	output: {
+		path: path.join(__dirname, "dist/"),
+		filename: "bundle.js"
+	}
+};
+
+gulp.task("default", ["webpack"]);
+
+gulp.task("webpack", function(callback) {
+	webpack(webpackOptions, function(err, stats) {
+		if(err) {
+			throw new gutil.PluginError("webpack", err);
+		}
+
+		gutil.log("[webpack]", stats.toString({
+			// output options
+		}));
+		callback();
+	});
+});
+
+gulp.task("serve", function(callback) {
+	const compiler = webpack(webpackOptions);
+
+	new webpackDevServer(compiler, {
+		// server and middleware options
+	}).listen(8080, "localhost", function(err) {
+		if(err) {
+			throw new gutil.PluginError("webpack-dev-server", err);
+		}
+		// Server listening
+		gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
+
+		// keep the server alive or continue?
+		// callback();
+	});
 });
