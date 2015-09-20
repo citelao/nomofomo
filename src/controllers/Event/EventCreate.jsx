eb338069b64113650c4ae08d40c50dfafb9a19ed
Ellen Sebastian <ellens2@stanford.edu>
2015-09-20 02:04:28 -0400

    add all needed inputs to create page

--- src/controllers/Event/EventCreate.jsx
+++ src/controllers/Event/EventCreate.jsx
@@ -13,8 +13,36 @@ export default class EventCreate extends React.Component {
 				Event location
 				<input type="text" id="create_location" />
 			</label>
+			<br />
 
-			(map here)
+			<label htmlFor="create_description">
+				Event description
+				<input type="text" id="create_description" />
+			</label>
+			<br />
+			<table> 
+				<tr>
+					<td className="third-width">
+						<label htmlFor="create_time">
+						Event time
+						<input type="time" id="create_time" />
+						</label>
+					</td>
+					<td className="third-width">
+						<label htmlFor="create_duration">
+						Event duration (hours)
+						<input type="text" id="create_duration" placeholder="1"/>
+						</label>
+					</td>
+					<td className="third-width">
+						<label htmlFor="min_duration">
+						Minimum attendance
+						<input type="text" id="min_duration" placeholder="3"/>
+						</label>
+					</td>
+				</tr>
+			</table>
+			<br />
 
 			<input type="submit" value="Create!"/>
 
